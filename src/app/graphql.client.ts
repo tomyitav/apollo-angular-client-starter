41b7e255729c9185b3c8826069bf8762a3d8fad7
Tom <tomyitav@gmail.com>
2017-05-29 01:33:02 +0300

    Finally configured subscriptions client

--- src/app/graphql.client.ts
+++ src/app/graphql.client.ts
@@ -1,9 +1,21 @@
 import {ApolloClient, createNetworkInterface} from "apollo-client";
+import {SubscriptionClient, addGraphQLSubscriptions} from 'subscriptions-transport-ws';
+
+let networkInterface = createNetworkInterface({
+  uri: 'http://localhost:8080/graphql'
+});
+
+const wsClient = new SubscriptionClient(`ws://localhost:8090/`, {
+  reconnect: true,
+});
+
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+  networkInterface,
+  wsClient
+);
 
 const client = new ApolloClient({
-  networkInterface: createNetworkInterface({
-    uri: 'http://localhost:8080/graphql'
-  }),
+  networkInterface: networkInterfaceWithSubscriptions
 });
 
 export function getClient(): ApolloClient {
