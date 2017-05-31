98fb8641fdee448d33720c15689506d852b423cd
Tom <tomyitav@gmail.com>
2017-05-31 12:39:22 +0300

    changing layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,7 @@ import { Http} from '@angular/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  myData: Array<any>
 
-  constructor(private http:Http) {
-    this.http.get('https://jsonplaceholder.typicode.com/photos')
-      .map(response => response.json())
-      .subscribe(res => this.myData = res);
+  constructor() {
   }
 }
