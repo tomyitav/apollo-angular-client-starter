f41748fc07f37a20638816da8b24f603c8a0c459
Tom <tomyitav@gmail.com>
2017-06-21 00:47:58 +0300

    improving toolbar directive to mark selected element

---
+++ src/app/trains/trains.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+  selector: 'app-trains',
+  templateUrl: './trains.component.html',
+  styleUrls: ['./trains.component.css']
+})
+export class TrainsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
